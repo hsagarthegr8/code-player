75d9f0710ce72f2fb1c22e11c58afb1820ea84e0
hsagarthegr8 <hsagarthegr8@gmail.com>
2020-06-06 16:26:13 +0530

    Minor Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { FC } from 'react'
 
-function App() {
+const App: FC = () => {
     return (
         <div className="App">
             <h1>Hello World</h1>
